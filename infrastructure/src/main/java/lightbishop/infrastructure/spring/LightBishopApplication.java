caeb0db1edc8a451e497437b1bd5a45de0b400d5
pmatysek <paulonio.blue@gmail.com>
2019-04-02 21:25:01 +0200

    Basic project structure closes #1

---
+++ infrastructure/src/main/java/lightbishop/infrastructure/spring/LightBishopApplication.java
@@ -0,0 +1,11 @@
+package lightbishop.infrastructure.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightBishopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LightBishopApplication.class);
+    }
+}
